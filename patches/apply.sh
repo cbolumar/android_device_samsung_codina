defc26b04553720a58a9538fd07313f37a7adc07
Shilin Victor <chrono.monochrome@gmail.com>
2015-05-24 15:44:15 +0300

    patcher: improve it - add colored output - reduce spam - prevent commiting patch with errors
    Change-Id: I38f953947e359b47d888ca5195feb4e58e453083

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,31 +1,54 @@
 LOCAL_PATH=../../../..
 
+export CL_RED="\033[31m"
+export CL_GRN="\033[32m"
+export CL_YLW="\033[33m"
+export CL_BLU="\033[34m"
+export CL_MAG="\033[35m"
+export CL_CYN="\033[36m"
+export CL_RST="\033[0m"
+
 pre_clean() {
     tmp=$PWD
     cd $1
 
-    git reset --hard > /dev/null
-    git clean -fd > /dev/null
-    git am  --abort > /dev/null
+    git reset --hard > /dev/null 2>&1
+    git clean -fd > /dev/null 2>&1
+    git am  --abort > /dev/null 2>&1
 
     cd $tmp
 
-    repo sync -fl $1
+    out=$( repo sync -fl $1 )
 }
 
 apply() {
-    patch -p1 -i $1
-    git commit -am "$1"
+    out=$( patch -p1 -i $1 )
+    fail=$( echo $out | grep -ic "FAILED" )
+    ign=$( echo $out | grep -ic "ignore" )
+
+    if [ "$fail" == "0" ]  ; then
+         git commit -am "$1"
+         if [ "$ign" != "0" ]  ; then
+             echo -e $CL_RED"some hunks of patch $1 has been ignored"$CL_RST
+             #echo -e $CL_RED$out$CL_RST | tr '.' '\n'
+         fi
+    else 
+         echo -e $CL_RED"patch $1 has been applied with errors"$CL_RST
+         #echo -e $CL_RED$l$CL_RST | tr '.' '\n'
+    fi;
 }
 
 apply_all() {
     tmp=$PWD
     cd $1
 
+    echo -e $CL_BLU"Applying patches to $1"$CL_RST ; echo "" ; echo "" ;
+
     for i in $( ls *.patch )
     do
-        echo "applying "$i" for "$1
+        echo "applying "$i
         apply $i
+        echo ""
     done
  
     cd $tmp
@@ -33,7 +56,7 @@ apply_all() {
 
 # pre clean 
 
-echo "get rid of any uncommitted or unstaged changes"
+echo -e $CL_GRN"get rid of any uncommitted or unstaged changes"$CL_RST
 
 patches=$PWD
 cd $LOCAL_PATH
